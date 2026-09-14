d9783273966f513e8f846f6bfedf796f37cf78e8
kongsni <binybinybiny03@gmail.com>
2026-09-14 12:34:59 +0900

    feat: Docker 및 MySQL 개발 환경 구성

---
+++ src/main/java/com/budzet/BudzetApplication.java
@@ -0,0 +1,13 @@
+package com.budzet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudzetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudzetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/budzet/BudzetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budzet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudzetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
