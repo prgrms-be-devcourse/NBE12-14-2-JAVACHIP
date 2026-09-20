6e1857baff165d7f006dfad00d0c344034e85ce4
kelly11998-gif <kelly11998@gmail.com>
2026-09-20 18:17:22 +0900

    feat : 예외처리 추가

--- back/src/main/java/com/budzet/domain/budget/service/BudgetChangeService.java
+++ back/src/main/java/com/budzet/domain/budget/service/BudgetChangeService.java
@@ -40,6 +40,13 @@ public class BudgetChangeService {
             throw new BusinessException(ErrorCode.FORBIDDEN_REQUEST_WRITER);
         }
 
+        //todo 관리자는 모든 맴버의 정산을 처리할 수 있는지 상의 후
+//        if(!(connection.getAuthority() == Authority.OWNER || connection.getAuthority() == Authority.OPERATOR)){
+//            if(!budgetRequest.getUser().getId().equals(userId)){
+//                throw new BusinessException(ErrorCode.FORBIDDEN_REQUEST_WRITER);
+//            }
+//        }
+
         //승인된 요청이 아닐 때 예외처리
         if(!"APPROVED".equals(budgetRequest.getStatus())){
             throw new BusinessException(ErrorCode.BUDGET_REQUEST_NOT_APPROVED);
