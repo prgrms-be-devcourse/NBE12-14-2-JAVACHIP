47ed2354b55f42d1472bfece0068bb441623e958
jeongsuji <sujijeong11@gmail.com>
2026-09-21 15:05:16 +0900

    refactor: PasswordEncoder 빈을 SecurityConfig로 통합

--- back/src/main/java/com/budzet/global/security/SecurityConfig.java
+++ back/src/main/java/com/budzet/global/security/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
@@ -53,5 +55,10 @@ public class SecurityConfig {
         ;
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
