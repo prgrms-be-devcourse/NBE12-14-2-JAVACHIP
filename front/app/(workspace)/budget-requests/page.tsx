9f5b47676be8bf0b3ca1114a23544ef0d1a7931d
Chocobe <kyw05171@gmail.com>
2026-09-21 18:26:02 +0900

    feat: 공통 레이아웃 구현

---
+++ front/app/(workspace)/budget-requests/page.tsx
@@ -0,0 +1,5 @@
+export default function DashboardPage() {
+  return <section aria-label="대시보드 콘텐츠">
+    예산 신청
+  </section>;
+}
---
+++ front/app/(workspace)/dashboard/page.tsx
@@ -0,0 +1,5 @@
+export default function DashboardPage() {
+  return <section aria-label="대시보드 콘텐츠">
+    대시보드 콘텐츠
+  </section>;
+}
